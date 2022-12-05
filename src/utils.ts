3a1c2558201d8395dc9a7bbd393740c94c89ed71
wizardfiction <17627585+wizardfiction@users.noreply.github.com>
2022-12-05 20:41:29 +1100

    fix: another linter fix

--- src/utils.ts
+++ src/utils.ts
@@ -2,12 +2,13 @@ import { PublicKey } from "@solana/web3.js";
 import { zip } from "lodash";
 
 export const sortByPubkey = (a: PublicKey, b: PublicKey) => {
-  let bytes: Uint8Array[] = zip(a.toBytes(), b.toBytes());
+  const bytes: Uint8Array[] = zip(a.toBytes(), b.toBytes());
   for (const byte of bytes) {
     return byte[0] < byte[1] ? [a, b] : [b, a]
   }
 };
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 export const decodeObjectToPubkeys = (json: { [key: string]: any }) => {
   for (const [key, value] of Object.entries(json)) {
